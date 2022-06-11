6579658d735ba4c1dec590b9f5d3b0718ff76690
Shubhi Gupta <83340187+Shubhivlog@users.noreply.github.com>
2022-06-11 12:48:12 +0530

    Add files via upload

---
+++ PreLoading of website/PreLoadingAction.js
@@ -0,0 +1,6 @@
+let Loader=document.getElementById("preloader");
+let container=document.getElementById("container");
+window.onload=()=>{
+container.style.display="block";
+Loader.style.display="none";
+}
